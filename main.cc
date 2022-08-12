b0a8282dfbe5f54756a7750993add83e288a93a4
mo-ja <iida.ryoya.ad@gmail.com>
2022-08-12 13:43:22 +0900

    add the body of main.cc for step1

--- main.cc
+++ main.cc
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main()
+int main(int argc, char** argv)
 {
+  
+  if (argc < 2){
+    fprintf(stderr, "[Error] The number of arguments must be two or over.");
+    return 1;
+  }
+
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+  printf("main:\n");
+  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  ret\n");
   return 0;
 }
